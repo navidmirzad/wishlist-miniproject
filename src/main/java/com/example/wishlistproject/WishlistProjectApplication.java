c227a2b27ae1d4865244d9573f50f7ef1143b2f8
navidmirzad <navi0221@stud.kea.dk>
2023-03-27 09:33:33 +0200

    first commit

---
+++ src/main/java/com/example/wishlistproject/WishlistProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishlistproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishlistProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wishlistproject/WishlistProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wishlistproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
